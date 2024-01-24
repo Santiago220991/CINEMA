52871df9c97a7143d7369020c526b1ad6fd2e9a9
Santiago220991 <alexander.cardenas10@outlook.com>
2024-01-23 20:32:33 -0500

    feat/SC: configure API

--- javascript/constants.js
+++ javascript/constants.js
@@ -3,7 +3,7 @@ export const API_MOVIE_ID_URL = 'https://moviesdatabase.p.rapidapi.com/titles/{i
 export const OPTIONS = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '<your api key>',
+		'X-RapidAPI-Key': '0488a8e143msh8f7370a057e3b0cp19d266jsn84bad44f3d70',
 		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
 	}
 };
